1f300ea981a0525ac6a3887ed110cb4aa8518179
Dayton Steffeny <daytonsteffeny@gmail.com>
2019-09-27 23:15:05 -0500

    Finished

--- Teleprompter/ViewController.swift
+++ Teleprompter/ViewController.swift
@@ -10,11 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tele: UITextView!
+    
+    var timer: Timer?
+    var counts = 0
+    var flip: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        counts = Int(tele.contentSize.height/(tele.font?.lineHeight)!)*4
+        
+        var blank = ""
+        for i in 1...counts/16 {
+            blank += "\n"
+        }
+ 
+        tele.text = blank + tele.text
+        tele.scrollRangeToVisible(NSMakeRange(0, 0))
+    }
+    
+    @objc func scroll(){
+        self.counts += 1
+        self.tele.scrollRangeToVisible(NSMakeRange(counts, 0))
     }
 
-
+    @IBAction func playButtonClick(_ sender: Any) {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(scroll), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func pauseButtonClick(_ sender: Any) {
+        timer?.invalidate()
+    }
+    
+    @IBAction func stopButtonClick(_ sender: Any) {
+        timer?.invalidate()
+        counts = Int(tele.bounds.height/(tele.font?.lineHeight)!)-100
+        self.tele.scrollRangeToVisible(NSMakeRange(0, 0))
+    }
+    
+    @IBAction func flipButtonClick(_ sender: Any) {
+        if flip {
+            tele.transform = CGAffineTransform.identity
+        } else {
+            tele.transform = CGAffineTransform(scaleX: -1, y: -1)
+        }
+         
+        flip = !flip
+    }
 }
+
 
